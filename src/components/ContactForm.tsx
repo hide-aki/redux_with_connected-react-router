43016a39786d137691083427a3afffe1814e6266
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-12-05 20:58:30 +0900

    refactor: Add Matrial-UI

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,35 +1,119 @@
-import * as React from 'react'
-import { Field, InjectedFormProps, reduxForm } from 'redux-form'
-
+import * as React from "react";
+import {
+  Field,
+  InjectedFormProps,
+  WrappedFieldInputProps,
+  reduxForm
+} from "redux-form";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 export interface ContactFormFields {
-  firstName?: string
-  lastName?: string
-  email?: string
+  firstName?: string;
+  lastName?: string;
+  email?: string;
 }
 
+const styles = (theme: any) => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    }
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  button: {
+    marginTop: theme.spacing.unit * 3,
+    marginLeft: theme.spacing.unit
+  }
+});
+
+const renderTextField = ({
+  input,
+  label,
+  meta: { touched, error },
+  children,
+  ...custom
+}: any) => {
+  return (
+    <TextField label={label} error={touched && error} {...input} {...custom} />
+  );
+};
 const PureContactForm: React.SFC<
   InjectedFormProps<ContactFormFields>
 > = props => {
-  const { handleSubmit } = props
+  const { handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
+    <React.Fragment>
+      <form onSubmit={handleSubmit}>
+        <Paper>
+          <Typography variant="h6" gutterBottom>
+            redux-form sample
+          </Typography>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <div>
+                <Field
+                  name="firstName"
+                  label="firstName"
+                  component={renderTextField}
+                  type="text"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <Field
+                  name="lastName"
+                  label="lastName"
+                  component={renderTextField}
+                  type="text"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <Field
+                  name="email"
+                  label="email"
+                  component={renderTextField}
+                  type="email"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <Button color="primary" type="submit">
+                  Submit
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </Paper>
+      </form>
+    </React.Fragment>
+  );
+};
 
 export const ContactForm = reduxForm({
-  form: 'contact',
-})(PureContactForm)
+  form: "contact"
+})(PureContactForm);
