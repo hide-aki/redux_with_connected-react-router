93bcebf5cd342055315ae85ce15e75cc2bd82e25
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-12-08 19:29:39 +0900

    chore: Add Matrial-UI to Preview.tsx

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,15 +1,54 @@
 import * as React from "react";
-
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Theme, Paper } from "@material-ui/core";
 export interface PreviewProps {
+  classes: {
+    layout: any;
+    paper: any;
+  };
   firstName?: string;
   lastName?: string;
   email?: string;
 }
 
-export const Preview = (props: PreviewProps) => (
-  <div>
-    <p>First Name: {props.firstName}</p>
-    <p>Last Name: {props.lastName}</p>
-    <p>E-mail: {props.email}</p>
-  </div>
+const styles = (theme: Theme) => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3
+    }
+  }
+});
+
+export const PurePreview = (props: PreviewProps) => (
+  <main className={props.classes.layout}>
+    <Paper className={props.classes.paper}>
+      <Typography gutterBottom variant="h5" component="p">
+        First Name: {props.firstName}
+      </Typography>
+      <Typography gutterBottom variant="h5" component="p">
+        Last Name: {props.lastName}
+      </Typography>
+      <Typography gutterBottom variant="h5" component="p">
+        E-mail: {props.email}
+      </Typography>
+    </Paper>
+  </main>
 );
+
+export const Preview = withStyles(styles)(PurePreview);
