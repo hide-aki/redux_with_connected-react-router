14062a8d0b501f38baaf3cd75897ae8b11a75c87
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-12-20 18:08:14 +0900

    fix

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { Field, InjectedFormProps, reduxForm } from "redux-form";
-import { ConsistentWith, Theme } from "@material-ui/core";
+import { Theme } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import withStyles from "@material-ui/core/styles/withStyles";
+import { createStyles, withStyles } from "@material-ui/core/styles";
 export interface ContactFormFields {
   firstName?: string;
   lastName?: string;
@@ -21,36 +21,37 @@ interface ContactFormStyles {
   };
 }
 
-const styles = (theme: Theme) => ({
-  layout: {
-    width: "auto",
-    marginLeft: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-      width: 600,
-      marginLeft: "auto",
-      marginRight: "auto"
+const styles = (theme: Theme) =>
+  createStyles({
+    layout: {
+      width: "auto",
+      marginLeft: theme.spacing.unit * 2,
+      marginRight: theme.spacing.unit * 2,
+      [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+        width: 600,
+        marginLeft: "auto",
+        marginRight: "auto"
+      }
+    },
+    paper: {
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 3,
+      padding: theme.spacing.unit * 2,
+      [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+        marginTop: theme.spacing.unit * 6,
+        marginBottom: theme.spacing.unit * 6,
+        padding: theme.spacing.unit * 3
+      }
+    },
+    buttons: {
+      display: "flex",
+      justifyContent: "flex-end"
+    },
+    button: {
+      marginTop: theme.spacing.unit * 3,
+      marginLeft: theme.spacing.unit
     }
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 3,
-    marginBottom: theme.spacing.unit * 3,
-    padding: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-      marginTop: theme.spacing.unit * 6,
-      marginBottom: theme.spacing.unit * 6,
-      padding: theme.spacing.unit * 3
-    }
-  },
-  buttons: {
-    display: "flex",
-    justifyContent: "flex-end"
-  },
-  button: {
-    marginTop: theme.spacing.unit * 3,
-    marginLeft: theme.spacing.unit
-  }
-});
+  });
 
 const renderTextField = ({
   input,
@@ -68,65 +69,61 @@ const PureContactForm: React.SFC<
 > = (props: InjectedFormProps<ContactFormFields> & ContactFormStyles) => {
   const { classes, handleSubmit } = props;
   return (
-    <React.Fragment>
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <React.Fragment>
-            <form onSubmit={handleSubmit}>
-              <Typography variant="h6" gutterBottom>
-                redux-form sample
-              </Typography>
-              <Grid container spacing={24}>
-                <Grid item xs={12}>
-                  <div>
-                    <Field
-                      name="firstName"
-                      label="firstName"
-                      component={renderTextField}
-                      type="text"
-                    />
-                  </div>
-                </Grid>
-                <Grid item xs={12}>
-                  <div>
-                    <Field
-                      name="lastName"
-                      label="lastName"
-                      component={renderTextField}
-                      type="text"
-                    />
-                  </div>
-                </Grid>
-                <Grid item xs={12}>
-                  <div>
-                    <Field
-                      name="email"
-                      label="email"
-                      component={renderTextField}
-                      type="email"
-                    />
-                  </div>
-                </Grid>
-                <Grid item xs={12}>
-                  <div>
-                    <Button
-                      className={classes.buttons}
-                      color="primary"
-                      type="submit"
-                    >
-                      Submit
-                    </Button>
-                  </div>
-                </Grid>
-              </Grid>
-            </form>
-          </React.Fragment>
-        </Paper>
-      </main>
-    </React.Fragment>
+    <main className={classes.layout}>
+      <Paper className={classes.paper}>
+        <form onSubmit={handleSubmit}>
+          <Typography variant="h6" gutterBottom>
+            redux-form sample
+          </Typography>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <div>
+                <Field
+                  name="firstName"
+                  label="firstName"
+                  component={renderTextField}
+                  type="text"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <Field
+                  name="lastName"
+                  label="lastName"
+                  component={renderTextField}
+                  type="text"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <Field
+                  name="email"
+                  label="email"
+                  component={renderTextField}
+                  type="email"
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div>
+                <Button
+                  className={classes.buttons}
+                  color="primary"
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </main>
   );
 };
 
 export const ContactForm = reduxForm({
   form: "contact"
-})(withStyles(styles)(PureContactForm) as any);
+})(withStyles(styles)(PureContactForm));
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Typography from "@material-ui/core/Typography";
-import withStyles from "@material-ui/core/styles/withStyles";
 import { Theme, Paper } from "@material-ui/core";
+import { createStyles, withStyles } from "@material-ui/core/styles";
 export interface PreviewProps {
   classes: {
     layout: any;
@@ -12,28 +12,29 @@ export interface PreviewProps {
   email?: string;
 }
 
-const styles = (theme: Theme) => ({
-  layout: {
-    width: "auto",
-    marginLeft: theme.spacing.unit * 2,
-    marginRight: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
-      width: 600,
-      marginLeft: "auto",
-      marginRight: "auto"
+const styles = (theme: Theme) =>
+  createStyles({
+    layout: {
+      width: "auto",
+      marginLeft: theme.spacing.unit * 2,
+      marginRight: theme.spacing.unit * 2,
+      [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+        width: 600,
+        marginLeft: "auto",
+        marginRight: "auto"
+      }
+    },
+    paper: {
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 3,
+      padding: theme.spacing.unit * 2,
+      [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+        marginTop: theme.spacing.unit * 6,
+        marginBottom: theme.spacing.unit * 6,
+        padding: theme.spacing.unit * 3
+      }
     }
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 3,
-    marginBottom: theme.spacing.unit * 3,
-    padding: theme.spacing.unit * 2,
-    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
-      marginTop: theme.spacing.unit * 6,
-      marginBottom: theme.spacing.unit * 6,
-      padding: theme.spacing.unit * 3
-    }
-  }
-});
+  });
 
 export const PurePreview = (props: PreviewProps) => (
   <main className={props.classes.layout}>
